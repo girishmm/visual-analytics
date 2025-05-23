3b90b7d59b983e64b6830f881be75e85a2d78bd3
girish <mohangiri1999@gmail.com>
2025-05-23 09:38:27 +0200

    Remove clutter and commented out template base

--- src/_server/preprocessing.js
+++ src/_server/preprocessing.js
@@ -3,20 +3,6 @@
  */
 
 /**
- * # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
- *
- * !!!!! Here an below, you can/should edit the code  !!!!!
- * - you can modify the data preprocessing functions
- * - you can add other data preprocessing functions for other functionalities
- *
- * # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
- */
-
-/**
- * My CODE STARTS
- */
-
-/**
  * Computes average user rating per mechanic.
  * Input: array of board game objects with 'mechanics' and 'average_user_rating'
  * Output: array of { mechanic: string, avg_rating: number }
@@ -86,56 +72,3 @@ export function encodeMechanicsLDA(data) {
 
   return { encoded, mechanicsList };
 }
-
-
-/**
- * My CODE ENDS and TEMPLATE starts
- */
-
-// /**
-//  * Returns boolean value, whether given row meets parameter conditions
-//  * @param {*} parameters
-//  * @param {*} row
-//  * @returns boolean
-//  */
-// export function is_below_max_weight(parameters, row) {
-//   return row.weight < parameters.max_weight
-// }
-
-// /**
-//  * Calculates the bmi for a specific person
-//  * @param {age, height, name, weight} person
-//  * @returns {age, bmi, height, name, weight}
-//  */
-// export function calc_bmi(person) {
-//   person.bmi = person.weight / ((person.height / 100) * (person.height / 100))
-//   return person
-// }
-
-// /**
-//  * Converts all attribute values to float, than can be converted
-//  * @param {*} obj
-//  * @returns {*}
-//  */
-// export function parse_numbers(obj) {
-//   for (const key in obj) {
-//     if (obj.hasOwnProperty(key)) {
-//       if (!isNaN(obj[key])) {
-//         obj[key] = parseFloat(obj[key])
-//       }
-//     }
-//   }
-//   return obj
-// }
-
-// /**
-//  * Test add function to demonstrate testing with jest in file preprocessing.test.js
-//  *
-//  * Adds the input numbers
-//  * @param {number} a
-//  * @param {number} b
-//  * @returns number
-//  */
-// export function test_func_add(a, b) {
-//   return a + b
-// }
--- src/_server/websocket.js
+++ src/_server/websocket.js
@@ -1,29 +1,11 @@
-//import * as csv from "csv-parser"
-
-import { parse } from "csv-parse";
 import * as fs from "fs"
 import { print_clientConnected, print_clientDisconnected } from "./static/utils.js"
-// const preprocessing = require("./preprocessing.js")
-// import { is_below_max_weight, parse_numbers, calc_bmi } from "./preprocessing.js"
-import { getExampleLDA } from "./druidExample.js";
-
-/**
- * My CODE STARTS
- */
 
 import { get_average_ratings_by_mechanic } from "./preprocessing.js"
 import { computeLDAProjection } from "./lda.js";
 
 const file_path = "data/"
 const file_name = "boardgames_100.json"
-
-/**
- * My CODE ENDS and TEMPLATE continues
- */
-
-
-// const file_path = "data/"
-// const file_name = "example_data.csv"
 
 /**
  * Does some console.logs when a client connected.
@@ -41,25 +23,7 @@ export function setupConnection(socket) {
   })
 
   /**
-   * # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-   *
-   * !!!!! Here an below, you can/should edit the code  !!!!!
-   * - you can modify the getData listener
-   * - you can add other listeners for other functionalities
-   *
-   * # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-   */
-
-
-  // module.exports = { setupWebSocket };
-  //  export { setupWebSocket };
-
-  /**
-   * My LISTENER STARTS
-   */
-
-  /**
-   * Listener to send mechanic rating averages to the client
+   * Listener to send mechanic rating averages to the client.
    */
   socket.on("getAverageRatingsByMechanic", () => {
     console.log("Client requested average ratings by mechanic")
@@ -78,6 +42,10 @@ export function setupConnection(socket) {
     console.log("Server emitted average ratings by mechanic")
   })
 
+  /**
+   * Listener to project via LDA and send to the client.
+   */
+
   socket.on("requestLDAProjection", async ({ targetDim }) => {
     console.log("Client requested LDA reduction")
 
@@ -89,56 +57,4 @@ export function setupConnection(socket) {
     socket.emit("ldaProjectionResult", projection);
     console.log("Server emitted LDA reduction")
   });
-
-
-  // /**
-  //  * My LISTENER ENDS and Template continues
-  //  */
-
-  // /**
-  //  * Listener that is called, if a message was sent with the topic "getData"
-  //  *
-  //  * In this case, the following is done:
-  //  * - Read in the data (.csv in this case) a a stream
-  //  *      (Stream -> data is read in line by line)
-  //  * - Do data preprocessing while reading in:
-  //  *      - Convert values, that can be represented as numbers to numbers
-  //  *      - Calculate the BMI for every data row (person)
-  //  *      - Filtering: if the row has a value, that contradicts the filtering parameters, data row will be excluded
-  //  *          (in this case: weight should not be larger than the max_weight filter-parameter)
-  //  */
-  // socket.on("getData", (obj) => {
-  //   console.log(`Data request with properties ${JSON.stringify(obj)}...`)
-
-  //   getExampleLDA(); //Example how to use druidjs. Just prints to the console for now
-
-
-  //   let parameters = obj.parameters
-
-  //   let jsonArray = []
-
-  //   // This is reading the .csv file line by line
-  //   // So we can filter it line by line
-  //   // This saves a lot of RAM and processing time
-  //   fs.createReadStream(file_path + file_name)
-  //     .pipe(parse({ delimiter: ',', columns: true }))
-  //     .on('data', function (row) {
-  //       row = parse_numbers(row)
-  //       row = calc_bmi(row)
-  //       // Filtering the data according the given parameter
-  //       // If it fits the parameter, add it to the result-array
-  //       let row_meets_criteria = is_below_max_weight(parameters, row)
-  //       if (row_meets_criteria) {
-  //         jsonArray.push(row)
-  //       }
-  //     })
-  //     .on("end", () => { //when all data is ready and processed, send it to the frontend of the socket
-  //       socket.emit("freshData", {
-  //         timestamp: new Date().getTime(),
-  //         data: jsonArray,
-  //         parameters: parameters,
-  //       })
-  //     })
-  //   console.log(`freshData emitted`)
-  // })
 }
